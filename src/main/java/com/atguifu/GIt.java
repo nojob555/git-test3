a28b2a7574e5f3d45445013fdd123e1c546f202c
fuck <fuck@123.com>
2022-11-03 22:30:54 +0800

    first commit

---
+++ src/main/java/com/atguifu/GIt.java
@@ -0,0 +1,13 @@
+package com.atguifu;
+
+/**
+ * @Auther: 19640
+ * @Date: 2022/11/3 22:21
+ * @Description:
+ */
+public class GIt {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
