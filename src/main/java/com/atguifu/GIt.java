3fffcb08770227eae531129a941954cc78ce776e
fuck <fuck@123.com>
2022-11-03 22:34:34 +0800

    second commit

--- src/main/java/com/atguifu/GIt.java
+++ src/main/java/com/atguifu/GIt.java
@@ -8,6 +8,7 @@ package com.atguifu;
 public class GIt {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 
 }
